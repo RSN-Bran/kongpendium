f569e4c39280ea9d566be75939249859afdfefa2
Bran <brandonsultana1@gmail.com>
2025-05-12 20:27:05 -0400

    Directory Changes

--- kongbot_discord.py
+++ kongbot_discord.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("KONGBOT_DISCORD_TOKEN")
 intents=discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
